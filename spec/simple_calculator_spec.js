ce8bf99eff433f3ea15012d1004ced35c574063d
SizweSeize <mpontshanesizwe@gmail.com>
2021-01-08 10:07:23 +0200

    updated_changes

--- spec/simple_calculator_spec.js
+++ spec/simple_calculator_spec.js
@@ -1,21 +1,25 @@
 const { add, multiply } = require("../src/simple_calculator");
 
 describe("add", function () {
-  it("should add two numbers", function () {
-    expect(add(3, 2)).toEqual(5);
+  it("should add two integers", function () {
+    expect(add(1, 2)).toEqual(3);
   });
-
-  it("should add numbers", function () {
+  it("should add two integers", function () {
+    expect(add(-1, -1)).toEqual(-2);
+  });
+  it("should add multiple integers", function () {
     expect(add(1, 2, 3, 4, 5)).toEqual(15);
   });
 });
 
 describe("multiply", function () {
-  it("should multiply two numbers", function () {
-    expect(multiply(2, 5)).toEqual(10);
+  it("should multiply two integers", function () {
+    expect(multiply(1, 3)).toEqual(3);
   });
-
-  it("should multiply numbers", function () {
+  it("should multiply two integers", function () {
+    expect(multiply(-1, 3)).toEqual(-3);
+  });
+  it("should multiply multiple integers", function () {
     expect(multiply(1, 2, 3, 4, 5)).toEqual(120);
   });
 });
--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -1,15 +1,15 @@
 function add() {
   var sum = 0;
-  for (i = 0; i < argument.length; i++) {
-    sum = sum + argument[i];
+  for (i = 0; i < arguments.length; i++) {
+    sum = sum + arguments[i];
   }
   return sum;
 }
 
 function multiply() {
   var product = 1;
-  for (i = 0; i < argument.length; i++) {
-    product = product * argument[i];
+  for (i = 0; i < arguments.length; i++) {
+    product = product * arguments[i];
   }
   return product;
 }
